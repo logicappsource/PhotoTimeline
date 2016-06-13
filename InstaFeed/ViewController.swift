e8ae9864812fe0111fe0bcfa88f6b62a269c5e5d
Patrick <patrick@dfsfs-macbook-pro.local>
2016-06-13 09:46:31 +0200

    Initial Commit

---
+++ InstaFeed/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  InstaFeed
+//
+//  Created by Patrick on 13/06/16.
+//  Copyright © 2016 logicappsource. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
