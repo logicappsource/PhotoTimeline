f3ee88fadd390c9d01d69bb2eb94d3cdf01e07b0
Patrick <patrick@dfsfs-macbook-pro.local>
2016-06-13 10:59:50 +0200

    Adding Photos to Collection View

---
+++ InstaFeed/ExploreCollectionViewController.swift
@@ -0,0 +1,88 @@
+//
+//  ExploreCollectionViewController.swift
+//  InstaFeed
+//
+//  Created by Patrick on 13/06/16.
+//  Copyright © 2016 logicappsource. All rights reserved.
+//
+
+import UIKit
+
+
+
+class ExploreCollectionViewController: UICollectionViewController {
+    
+    
+    struct Storyboard {
+        static let CellIdentifier = "ExplorePhotoCell"
+    }
+    
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Register cell classes
+        //self.collectionView!.registerClass(UICollectionViewCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    
+    override func numberOfSectionsInCollectionView(collectionView: UICollectionView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+
+
+    override func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of items
+        return 10
+    }
+
+    override func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier(Storyboard.CellIdentifier, forIndexPath: indexPath)
+        as! ExplorePhotoCollectionViewCell
+    
+        // Configure the cell
+        
+        cell.imageView.image = UIImage(named: "Screen Shot 2016-06-09 at 23.25.18")
+        return cell
+    }
+
+    // MARK: UICollectionViewDelegate
+
+    /*
+    // Uncomment this method to specify if the specified item should be highlighted during tracking
+    override func collectionView(collectionView: UICollectionView, shouldHighlightItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment this method to specify if the specified item should be selected
+    override func collectionView(collectionView: UICollectionView, shouldSelectItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return true
+    }
+    */
+
+    /*
+    // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+    override func collectionView(collectionView: UICollectionView, shouldShowMenuForItemAtIndexPath indexPath: NSIndexPath) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, canPerformAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) -> Bool {
+        return false
+    }
+
+    override func collectionView(collectionView: UICollectionView, performAction action: Selector, forItemAtIndexPath indexPath: NSIndexPath, withSender sender: AnyObject?) {
+    
+    }
+    */
+
+}
---
+++ InstaFeed/ExplorePhotoCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  ExplorePhotoCollectionViewCell.swift
+//  InstaFeed
+//
+//  Created by Patrick on 13/06/16.
+//  Copyright © 2016 logicappsource. All rights reserved.
+//
+
+import UIKit
+
+class ExplorePhotoCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var imageView: UIImageView!
+}
